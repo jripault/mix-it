ca773b5db72982288108d7049c3d9dc14987c374
Julien Ripault <tluapir@gmail.com>
2011-11-25 10:06:35 +0100

    Last version

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,37 +4,36 @@ import models.Attendees;
 import models.Favorite;
 import play.mvc.Controller;
 
-public class Application extends Controller
-{
+public class Application extends Controller {
 
-  public static void index()
-  {
-    Attendees attendees = Attendees.findAll();
-    renderArgs.put("attendees", attendees);
-    render();
-  }
+    public static void index() {
+        /*Attendees attendees = Attendees.findAll();
+        renderArgs.put("attendees", attendees);
+        render();*/
+        renderTemplate("Application/next.html");
+    }
 
-  public static void description()
-  {
-    renderTemplate("Application/description.html");
-  }
+    public static void description() {
+        renderTemplate("Application/description.html");
+    }
 
-  public static void team()
-  {
-    renderTemplate("Application/team.html");
-  }
+    public static void questionnaire() {
+        renderTemplate("Application/questionnaire.html");
+    }
 
-  public static void inscription()
-  {
-    Attendees attendees = Attendees.findAll();
-    renderArgs.put("attendees", attendees);
-    renderTemplate("Application/inscription.html");
-  }
+    public static void team() {
+        renderTemplate("Application/team.html");
+    }
 
-  public static void stats()
-  {
-    String stats = Favorite.getStats();
-    renderArgs.put("stats", stats);
-    renderTemplate("Application/stats.html");
-  }
+    public static void inscription() {
+        Attendees attendees = Attendees.findAll();
+        renderArgs.put("attendees", attendees);
+        renderTemplate("Application/inscription.html");
+    }
+
+    public static void stats() {
+        String stats = Favorite.getStats();
+        renderArgs.put("stats", stats);
+        renderTemplate("Application/stats.html");
+    }
 }
